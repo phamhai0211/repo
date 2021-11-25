cde77477b749ea4dbe7b2acc8106c679a089e75e
phamhai0211 <haidb119@gmail.com>
2021-11-25 23:04:41 +0700

    push

---
+++ testjs/main.js
@@ -0,0 +1,12 @@
+let car = []
+   
+
+let maXe = document.getElementById('1')
+let tenXe = document.getElementById('2')
+let car1 = "123"
+let car2 = "adac"
+function actionCar() {
+    let oj = car.push(maXe.value,tenXe.value)
+    console.log(car)
+}
+
